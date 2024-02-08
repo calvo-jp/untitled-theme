6f2ef41ab32f6e2db201e9fd3465964bf1a12bd6
jp calvo <calvojp92@gmail.com>
2024-02-08 22:14:24 +0800

    initial button

--- src/app/(recipes)/page.tsx
+++ src/app/(recipes)/page.tsx
@@ -1,5 +1,12 @@
-import {styled} from '@/styled-system/jsx';
+import {Button} from '@/components/button';
+import {Flex} from '@/styled-system/jsx';
 
 export default function Recipes() {
-  return <styled.div />;
+  return (
+    <Flex p={4} gap={2}>
+      <Button size="sm" variant="outline" disabled>
+        Click me
+      </Button>
+    </Flex>
+  );
 }
