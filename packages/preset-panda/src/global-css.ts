0cd70a008edff68ab4139b7a22d8885ed36161f5
jp calvo <calvojp92@gmail.com>
2024-02-14 22:01:26 +0800

    add global css

--- packages/preset-panda/src/global-css.ts
+++ packages/preset-panda/src/global-css.ts
@@ -1,3 +1,25 @@
 import {defineGlobalStyles} from '@pandacss/dev';
 
-export const globalCss = defineGlobalStyles({});
+export const globalCss = defineGlobalStyles({
+	html: {
+		lineHeight: 1.5,
+		MozOsxFontSmoothing: 'grayscale',
+		textRendering: 'optimizeLegibility',
+		WebkitFontSmoothing: 'antialiased',
+		WebkitTextSizeAdjust: '100%',
+		scrollBehavior: 'smooth',
+	},
+	body: {
+		bg: 'white',
+		color: 'gray.light.800',
+	},
+	'*, *::before, *::after': {
+		borderColor: 'gray.light.300',
+		borderStyle: 'solid',
+		boxSizing: 'border-box',
+	},
+	'*::placeholder': {
+		opacity: 1,
+		color: 'gray.light.500',
+	},
+});
