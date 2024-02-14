df55acbff69cc1857366c67e00967293d075ed04
jp calvo <calvojp92@gmail.com>
2024-02-14 17:46:02 +0800

    fix

--- examples/next-js/panda.config.ts
+++ examples/next-js/panda.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from '@pandacss/dev';
 
 export default defineConfig({
 	preflight: true,
-	presets: ['@pandacss/preset-base', '@untitled-theme/preset'],
+	presets: ['@pandacss/preset-base', '@untitled-theme/preset-panda'],
 	include: ['./src/**/*.{ts,tsx,js,jsx}'],
 	exclude: [],
 	theme: {
