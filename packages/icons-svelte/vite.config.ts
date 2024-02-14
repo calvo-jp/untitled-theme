084eddd056a4c25c20927597479665a0a9f25ed6
jp calvo <calvojp92@gmail.com>
2024-02-14 17:03:38 +0800

    svelte build

---
+++ packages/icons-svelte/vite.config.ts
@@ -0,0 +1,15 @@
+import {svelte} from '@sveltejs/vite-plugin-svelte';
+import {defineConfig} from 'vite';
+
+export default defineConfig({
+	plugins: [
+		svelte({
+			hot: false,
+		}),
+	],
+	build: {
+		rollupOptions: {
+			external: ['svelte'],
+		},
+	},
+});
