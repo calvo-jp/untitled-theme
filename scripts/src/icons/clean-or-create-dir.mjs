05e6f8bcc4abded5fedfe21edaa1ee694e74b110
jp calvo <calvojp92@gmail.com>
2024-02-18 10:31:13 +0800

    refactor

--- scripts/src/icons/clean-or-create-dir.mjs
+++ scripts/src/icons/clean-or-create-dir.mjs
@@ -6,11 +6,6 @@ import fs from 'fs/promises';
  * @param {string} location
  */
 export async function clean_or_create_dir(location) {
-	try {
-		await fs.rm(location, {force: true, recursive: true});
-	} catch {
-		//
-	} finally {
-		await fs.mkdir(location, {recursive: true});
-	}
+	await fs.rm(location, {force: true, recursive: true});
+	await fs.mkdir(location, {recursive: true});
 }
