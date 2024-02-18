134aec7e312fabe394e05928116bdf0c62149829
jp calvo <calvojp92@gmail.com>
2024-02-18 16:36:50 +0800

    update svelte config

--- packages/icons-svelte/vite.config.ts
+++ packages/icons-svelte/vite.config.ts
@@ -2,11 +2,7 @@ import {svelte} from '@sveltejs/vite-plugin-svelte';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-	plugins: [
-		svelte({
-			hot: false,
-		}),
-	],
+	plugins: [svelte()],
 	build: {
 		rollupOptions: {
 			external: ['svelte'],
