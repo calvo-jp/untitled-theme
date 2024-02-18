d872b671208b4da0c5eaf2d8ec828de8b5a65812
jp calvo <calvojp92@gmail.com>
2024-02-18 22:07:30 +0800

    use vite

---
+++ packages/icons-react/vite.config.mjs
@@ -0,0 +1,18 @@
+import path from 'path';
+import {defineConfig} from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+	plugins: [dts()],
+	build: {
+		lib: {
+			name: 'icons-react',
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			formats: ['es'],
+			fileName: 'index',
+		},
+		rollupOptions: {
+			external: ['react'],
+		},
+	},
+});
