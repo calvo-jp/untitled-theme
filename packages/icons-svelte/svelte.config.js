b92eb2d9fd1c7953281a050e1ac97e3af22b877a
jp calvo <calvojp92@gmail.com>
2024-02-18 16:42:28 +0800

    add types to config

--- packages/icons-svelte/svelte.config.js
+++ packages/icons-svelte/svelte.config.js
@@ -1,7 +1,12 @@
+// @ts-check
+
 import sveltePreprocess from 'svelte-preprocess';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	preprocess: sveltePreprocess({
 		typescript: true,
 	}),
 };
+
+export default config;
