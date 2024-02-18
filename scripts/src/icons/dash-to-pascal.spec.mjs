9fa488ed94594e0a45fc8d61beb8ab3fee39747d
jp calvo <calvojp92@gmail.com>
2024-02-18 12:55:43 +0800

    change to spec

--- scripts/src/icons/dash-to-pascal.spec.mjs
+++ scripts/src/icons/dash-to-pascal.spec.mjs
@@ -4,7 +4,7 @@ import {describe, expect, it} from 'vitest';
 import {dash_to_pascal} from './dash-to-pascal.mjs';
 
 describe('dash_to_pascal', () => {
-	it('converts a dash-case string to PascalCase', () => {
+	it('converts a dash cased string to pascal case', () => {
 		expect(dash_to_pascal('dash-to-pascal')).toBe('DashToPascal');
 	});
 });
