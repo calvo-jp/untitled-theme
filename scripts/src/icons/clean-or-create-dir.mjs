c607ec0a7419b1219312b484a846e6a386daa169
jp calvo <calvojp92@gmail.com>
2024-02-18 10:29:45 +0800

    refactor

--- scripts/src/icons/clean-or-create-dir.mjs
+++ scripts/src/icons/clean-or-create-dir.mjs
@@ -1,16 +1,19 @@
 // @ts-check
 
 import fs from 'fs/promises';
-import path from 'path';
 
 /**
  * @param {string} location
  */
 export async function clean_or_create_dir(location) {
 	try {
-		const files = await fs.readdir(location);
-		await Promise.allSettled(files.map((file) => fs.unlink(path.join(location, file))));
+		await fs.rm(location, {
+			force: true,
+			recursive: true,
+		});
 	} catch {
+		//
+	} finally {
 		await fs.mkdir(location);
 	}
 }
--- scripts/src/icons/generate-icons.mjs
+++ scripts/src/icons/generate-icons.mjs
@@ -5,10 +5,10 @@ import {generate_icons_react} from './generate-icons-react.mjs';
 import {generate_icons_svelte} from './generate-icons-svelte.mjs';
 
 async function generate_icons() {
-	const spinner = p.spinner();
+	p.intro('Generate icons');
 
 	const frameworks = await p.multiselect({
-		message: 'Select frameworks to generate icons for:',
+		message: 'Select frameworks:',
 		options: [
 			{
 				value: 'react',
@@ -22,24 +22,29 @@ async function generate_icons() {
 		required: true,
 	});
 
-	spinner.start('Generating icons');
+	if (p.isCancel(frameworks)) {
+		p.cancel('Cancelled');
+		process.exit(0);
+	}
 
 	/**
 	 * @type {Promise<void>[]}
 	 */
 	const promises = [];
+	const spinner = p.spinner();
 
-	if (Array.isArray(frameworks)) {
-		if (frameworks.includes('react')) promises.push(generate_icons_react());
-		if (frameworks.includes('svelte')) promises.push(generate_icons_svelte());
-	}
+	spinner.start('Generating icons');
+
+	if (frameworks.includes('react')) promises.push(generate_icons_react());
+	if (frameworks.includes('svelte')) promises.push(generate_icons_svelte());
 
 	try {
 		await Promise.all(promises);
 	} catch {
-		spinner.message('Something went wrong generating icons');
+		spinner.message('Something went wrong');
 	} finally {
-		spinner.stop('Done');
+		spinner.stop('Icons generated');
+		p.outro('Goodbye!');
 	}
 }
 
