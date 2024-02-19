b8f49c9ddee3e7b0ad7c565ff6537c13ce127f57
jp calvo <calvojp92@gmail.com>
2024-02-19 19:57:59 +0800

    refactor script

--- scripts/src/icons/format.mjs
+++ scripts/src/icons/format.mjs
@@ -3,18 +3,26 @@
 import path from 'path';
 import prettier from 'prettier';
 
-const config = await prettier.resolveConfig(path.join(process.cwd(), '.prettierrc'));
+let config = null;
+
+async function get_config() {
+	if (config) return config;
+
+	const c = await prettier.resolveConfig(path.join(process.cwd(), '.prettierrc'));
+	config = c;
+	return c;
+}
 
 /**
  * @param {string} content
  */
 export async function format_html(content) {
-	return await prettier.format(content, {...config, parser: 'html'});
+	return await prettier.format(content, {...(await get_config()), parser: 'html'});
 }
 
 /**
  * @param {string} content
  */
 export async function format_ts(content) {
-	return await prettier.format(content, {...config, parser: 'typescript'});
+	return await prettier.format(content, {...(await get_config()), parser: 'typescript'});
 }
