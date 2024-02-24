4231a4f9f17d315da036d375bf7b8515e2aa5db2
jp calvo <calvojp92@gmail.com>
2024-02-24 11:36:47 +0800

    fix input

--- packages/preset-panda/src/pages/input.tsx
+++ packages/preset-panda/src/pages/input.tsx
@@ -1,5 +1,15 @@
+import {Button} from '@/components/button';
 import {Input} from '@/components/input';
+import {Box} from 'styled-system/jsx';
 
 export default function InputPage() {
-	return <Input />;
+	return (
+		<Box maxW="22rem">
+			<Input w="full" size="lg" placeholder="Username" data-invalid />
+			<Input w="full" size="lg" mt={5} placeholder="Password" value="Password" />
+			<Button w="full" size="lg" mt={8}>
+				Submit
+			</Button>
+		</Box>
+	);
 }
