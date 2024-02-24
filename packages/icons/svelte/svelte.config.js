eecdb3b8705ee6a33017680e10cd8956eafe845f
jp calvo <calvojp92@gmail.com>
2024-02-24 19:55:28 +0800

    rm deps

--- packages/icons/svelte/svelte.config.js
+++ packages/icons/svelte/svelte.config.js
@@ -1,8 +1,5 @@
-// @ts-check
-
 import sveltePreprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: sveltePreprocess({
 		typescript: true,
