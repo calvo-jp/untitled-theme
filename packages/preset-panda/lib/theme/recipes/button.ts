276d96c29268d9f5b14b6c6f1130c633ad6293dd
jp calvo <calvojp92@gmail.com>
2024-02-24 11:36:55 +0800

    init button

--- packages/preset-panda/lib/theme/recipes/button.ts
+++ packages/preset-panda/lib/theme/recipes/button.ts
@@ -30,12 +30,12 @@ export const button = defineRecipe({
 					bg: 'colorPalette.700',
 				},
 				_focus: {
-					boxShadow: '0 0 0 3px var(--shadow-color)',
+					boxShadow: '0 0 0 4px var(--shadow-color)',
 					boxShadowColor: 'colorPalette.500/25',
 				},
 			},
 			outline: {
-				colorPalette: 'gray.dark',
+				colorPalette: 'gray-dark',
 				borderWidth: '1px',
 				borderStyle: 'solid',
 				borderColor: 'colorPalette.300',
@@ -45,7 +45,7 @@ export const button = defineRecipe({
 					color: 'colorPalette.700',
 				},
 				_focus: {
-					boxShadow: '0 0 0 3px var(--shadow-color)',
+					boxShadow: '0 0 0 4px var(--shadow-color)',
 					boxShadowColor: 'colorPalette.900/5',
 				},
 			},
@@ -60,33 +60,21 @@ export const button = defineRecipe({
 		},
 		size: {
 			sm: {
-				h: '2.25rem',
-				px: '0.75rem',
-				gap: '0.25rem',
-				fontSize: 'sm',
-			},
-			md: {
 				h: '2.5rem',
 				px: '0.875rem',
 				gap: '0.25rem',
 				fontSize: 'sm',
 			},
-			lg: {
+			md: {
 				h: '2.75rem',
 				px: '1rem',
 				gap: '0.375rem',
 				fontSize: 'md',
 			},
-			xl: {
+			lg: {
 				h: '3rem',
 				px: '1.125rem',
 				gap: '0.375rem',
-				fontSize: 'md',
-			},
-			'2xl': {
-				h: '3.75rem',
-				px: '1.375rem',
-				gap: '0.625rem',
 				fontSize: 'lg',
 			},
 		},
--- packages/preset-panda/src/pages/button.tsx
+++ packages/preset-panda/src/pages/button.tsx
@@ -1,5 +1,12 @@
 import {Button} from '@/components/button';
+import {HStack} from 'styled-system/jsx';
 
 export default function ButtonPage() {
-	return <Button>Click me</Button>;
+	return (
+		<HStack gap={4}>
+			<Button>Click me</Button>
+			<Button variant="outline">Click me</Button>
+			<Button variant="ghost">Click me</Button>
+		</HStack>
+	);
 }
