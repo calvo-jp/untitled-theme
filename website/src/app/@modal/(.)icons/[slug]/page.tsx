91e3a689bf0892c0bfd4f71aad709662be6d162c
jp calvo <calvojp92@gmail.com>
2024-02-29 19:47:19 +0800

    intercept routes enhancement

--- website/src/app/@modal/(.)icons/[slug]/page.tsx
+++ website/src/app/@modal/(.)icons/[slug]/page.tsx
@@ -1,6 +1,7 @@
+import {IconDetails} from '@/app/icon-details';
 import {getIcon} from '@/app/utils';
-import {Dialog} from '@ark-ui/react';
-import {XCloseIcon} from '@untitled-theme/icons-react';
+import {notFound} from 'next/navigation';
+import {Modal} from './modal';
 
 interface Props {
   params: {
@@ -11,18 +12,11 @@ interface Props {
 export default async function IconDetailsModal({params}: Props) {
   const icon = await getIcon(params.slug);
 
-  return (
-    <Dialog.Root open lazyMount unmountOnExit>
-      <Dialog.Backdrop className="fixed inset-0 z-overlay bg-white/25 backdrop-blur-sm data-open:animate-fade-in data-closed:animate-fade-out dark:bg-gray-true-950/50" />
-      <Dialog.Positioner>
-        <Dialog.Content className="fixed bottom-0 right-0 z-modal w-full border-t bg-white p-4 data-open:animate-slide-up data-closed:animate-slide-down md:p-6 lg:p-8 dark:bg-gray-true-950">
-          <div>It works</div>
+  if (!icon) return notFound();
 
-          <Dialog.CloseTrigger className="absolute right-3 top-3 p-1">
-            <XCloseIcon />
-          </Dialog.CloseTrigger>
-        </Dialog.Content>
-      </Dialog.Positioner>
-    </Dialog.Root>
+  return (
+    <Modal>
+      <IconDetails data={icon} />
+    </Modal>
   );
 }
--- website/src/app/icons/[slug]/page.tsx
+++ website/src/app/icons/[slug]/page.tsx
@@ -1,6 +1,5 @@
+import {IconDetails} from '@/app/icon-details';
 import {getIcon} from '@/app/utils';
-import {Clipboard, Tabs} from '@ark-ui/react';
-import {CheckIcon, Copy01Icon} from '@untitled-theme/icons-react';
 import {notFound} from 'next/navigation';
 
 interface Props {
@@ -12,93 +11,5 @@ interface Props {
 export default async function IconDetailsPage({params}: Props) {
   const icon = await getIcon(params.slug);
 
-  if (!icon) return notFound();
-
-  const items = [
-    {
-      label: 'Html',
-      value: 'Html',
-      content: icon.snippet.html,
-    },
-    {
-      label: 'React',
-      value: 'React',
-      content: icon.snippet.react,
-    },
-    {
-      label: 'Svelte',
-      value: 'Svelte',
-      content: icon.snippet.svelte,
-    },
-  ];
-
-  return (
-    <div>
-      <div
-        className="w-fit rounded border p-3"
-        dangerouslySetInnerHTML={{
-          __html: icon.html,
-        }}
-      />
-
-      <div className="mt-5 flex max-h-screen w-fit items-center gap-5 overflow-y-auto rounded-sm bg-gray-true-100 px-3 py-2 dark:bg-gray-true-800/25">
-        <code>&lt;{icon.name}&nbsp;&#47;&gt;</code>
-
-        <Clipboard.Root className="flex">
-          <Clipboard.Label className="sr-only">Copy</Clipboard.Label>
-          <Clipboard.Trigger className="p-1">
-            <Clipboard.Indicator
-              copied={<CheckIcon className="h-4 w-4 text-success-500 dark:text-success-700" />}
-            >
-              <Copy01Icon className="h-4 w-4" />
-            </Clipboard.Indicator>
-          </Clipboard.Trigger>
-        </Clipboard.Root>
-      </div>
-
-      <Tabs.Root className="mt-5" defaultValue={items[0].value}>
-        <Tabs.List className="flex gap-4">
-          {items.map((item) => (
-            <Tabs.Trigger
-              key={item.value}
-              value={item.value}
-              className="transition-colors duration-300 data-selected:text-brand-700 dark:data-selected:text-brand-500"
-            >
-              {item.label}
-            </Tabs.Trigger>
-          ))}
-        </Tabs.List>
-
-        {items.map((item) => {
-          return (
-            <Tabs.Content key={item.value} value={item.value} asChild>
-              <div className="relative mt-5 rounded-sm text-sm">
-                <Clipboard.Root
-                  value={item.content}
-                  className="absolute right-4 top-4 flex items-center justify-center rounded-lg bg-white/50 p-0.5 backdrop-blur-sm dark:bg-gray-true-950/50"
-                >
-                  <Clipboard.Label className="sr-only">Copy</Clipboard.Label>
-                  <Clipboard.Trigger className="p-0.5">
-                    <Clipboard.Indicator
-                      copied={
-                        <CheckIcon className="h-5 w-5 text-success-500 dark:text-success-700" />
-                      }
-                    >
-                      <Copy01Icon className="h-5 w-5" />
-                    </Clipboard.Indicator>
-                  </Clipboard.Trigger>
-                </Clipboard.Root>
-
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: item.content,
-                  }}
-                />
-              </div>
-            </Tabs.Content>
-          );
-        })}
-      </Tabs.Root>
-    </div>
-  );
+  return !icon ? notFound() : <IconDetails data={icon} />;
 }
