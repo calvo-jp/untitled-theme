ffa41b041ef868f6d2e32c4aa8463bb3899e9f5d
jp calvo <calvojp92@gmail.com>
2024-02-29 20:43:24 +0800

    fix shiki height on modal

--- website/src/app/@modal/(.)icons/[slug]/modal.tsx
+++ website/src/app/@modal/(.)icons/[slug]/modal.tsx
@@ -20,7 +20,7 @@ export function Modal({children}: PropsWithChildren) {
       <Dialog.Backdrop className="fixed inset-0 z-overlay bg-white/25 backdrop-blur-sm data-open:animate-fade-in data-closed:animate-fade-out dark:bg-gray-true-950/50" />
       <Dialog.Positioner>
         <Dialog.Content className="fixed bottom-0 right-0 z-modal w-full border-t bg-white p-4 data-open:animate-slide-up data-closed:animate-slide-down md:p-6 lg:p-8 dark:bg-gray-true-950">
-          {children}
+          <div className="max-w-lg [&_.shiki]:max-h-[48vh]">{children}</div>
 
           <Dialog.CloseTrigger className="absolute right-3 top-3 p-1">
             <XCloseIcon />
