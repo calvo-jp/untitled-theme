533c82688f3632d86b80a8466d29c24e8a070461
jp calvo <calvojp92@gmail.com>
2024-03-01 13:13:51 +0800

    update website deps

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['@untitled-theme/icons-react', '@ark-ui/react'],
+    serverMinification: true,
+  },
+};
 
 export default nextConfig;
