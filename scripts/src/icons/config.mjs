19fa3d9b7d9af554cbeb4829c33c9cc3efdc2dd0
jp calvo <calvojp92@gmail.com>
2024-03-01 15:58:03 +0800

    move stroke and stroke-width to parent

--- scripts/src/icons/config.mjs
+++
@@ -1,9 +0,0 @@
-export const config = {
-  width: '24',
-  height: '24',
-  stroke: 'currentColor',
-  strokeWidth: '2',
-  ariaHidden: 'true',
-  viewBox: '0 0 24 24',
-  className: 'untitled-icon',
-};
