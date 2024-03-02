6f85e759f8bd47f8ef7aa4c230a56d042a2e8bde
jp calvo <calvojp92@gmail.com>
2024-03-02 17:31:07 +0800

    stream icon details

---
+++ website/src/app/@modal/(.)icons/[slug]/layout.tsx
@@ -0,0 +1,6 @@
+import {type PropsWithChildren} from 'react';
+import {Modal} from './modal';
+
+export default function Layout({children}: PropsWithChildren) {
+  return <Modal>{children}</Modal>;
+}
--- website/src/app/@modal/(.)icons/[slug]/page.tsx
+++ website/src/app/@modal/(.)icons/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import {IconDetails} from '@/app/icon-details';
 import {getIcon, getIcons} from '@/app/utils';
 import {notFound} from 'next/navigation';
-import {Modal} from './modal';
 
 export async function generateStaticParams() {
   const icons = await getIcons();
@@ -19,9 +18,5 @@ export default async function IconDetailsModal({params}: Props) {
 
   if (!icon) return notFound();
 
-  return (
-    <Modal>
-      <IconDetails data={icon} />
-    </Modal>
-  );
+  return <IconDetails data={icon} />;
 }
