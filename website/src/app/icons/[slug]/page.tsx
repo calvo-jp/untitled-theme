8a3a3b10df6350299e70efbdc0befb7cda773cc9
jp calvo <calvojp92@gmail.com>
2024-03-02 18:08:04 +0800

    refactor metadata

--- website/src/app/icons/[slug]/page.tsx
+++ website/src/app/icons/[slug]/page.tsx
@@ -1,10 +1,19 @@
 import {IconDetails} from '@/app/icon-details';
 import {getIcon, getIcons} from '@/app/utils';
+import {type Metadata} from 'next';
 import {notFound} from 'next/navigation';
 
 export async function generateStaticParams() {
   const icons = await getIcons();
   return icons.map(({slug}) => ({slug}));
+}
+
+export async function generateMetadata({params}: Props): Promise<Metadata> {
+  const icon = await getIcon(params.slug);
+
+  return {
+    title: icon?.name.pascal,
+  };
 }
 
 interface Props {
