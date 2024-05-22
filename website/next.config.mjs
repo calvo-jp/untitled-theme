fd50fba52cced50d5de209508b70183e383d9a8c
jp calvo <calvojp92@gmail.com>
2024-05-22 11:17:03 +0800

    update next config

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     optimizePackageImports: ['@untitled-theme/icons-react', '@ark-ui/react'],
     serverMinification: true,
