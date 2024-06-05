12fe5ab4a0262ca0a66152a2adad7bc2c0981401
jp calvo <calvojp92@gmail.com>
2024-06-05 09:13:42 +0800

    format all

--- packages/core/colors/src/types.ts
+++ packages/core/colors/src/types.ts
@@ -1,7 +1,7 @@
 export interface ColorPalette {
-  [key: string]:
-    | string
-    | {
-        [key: string]: string;
-      };
+	[key: string]:
+		| string
+		| {
+				[key: string]: string;
+		  };
 }
--- packages/icons/svelte/vite.config.ts
+++ packages/icons/svelte/vite.config.ts
@@ -2,12 +2,12 @@ import {svelte} from '@sveltejs/vite-plugin-svelte';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    /* issue: https://github.com/sveltejs/kit/issues/7792 */
-    sourcemap: 'inline',
-    rollupOptions: {
-      external: ['svelte'],
-    },
-  },
+	plugins: [svelte()],
+	build: {
+		/* issue: https://github.com/sveltejs/kit/issues/7792 */
+		sourcemap: 'inline',
+		rollupOptions: {
+			external: ['svelte'],
+		},
+	},
 });
