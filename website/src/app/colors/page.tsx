d183bcad0dc803e7da93671a914324e3c18b748d
jp calvo <calvojp92@gmail.com>
2024-06-23 21:25:54 +0800

    feat: copy color as json

--- website/src/app/colors/page.tsx
+++ website/src/app/colors/page.tsx
@@ -1,5 +1,8 @@
+import {Clipboard, ClipboardIndicator, ClipboardTrigger} from '@/lib/clipboard';
+import {Icon} from '@/lib/icon';
 import {Box, Flex, Grid} from '@/styled-system/jsx';
 import colors from '@untitled-theme/colors';
+import {CheckIcon, Copy01Icon} from '@untitled-theme/icons-react';
 import type {Metadata} from 'next';
 import {Suspense} from 'react';
 import {Searchbar} from './searchbar';
@@ -33,68 +36,108 @@ export default function Page({searchParams}: Props) {
 				}}
 			>
 				{items.map((item) => (
-					<Flex
-						key={item.label}
-						flexDir={{
-							base: 'column',
-						}}
-						gap={{
-							base: '3',
-							lg: '2',
-						}}
-					>
-						<Box w="8rem" fontWeight="medium">
-							{item.label}
-						</Box>
-
-						<Grid
-							gridTemplateColumns={{
-								base: 4,
-								md: 8,
-								lg: 12,
-							}}
-							gap="3"
-						>
-							{item.value.map((subItem) => (
-								<Box key={subItem.label}>
-									<Box
-										aspectRatio="square"
-										style={{
-											backgroundColor: subItem.value,
-										}}
-									/>
-
-									<Box mt="2" fontSize="sm">
-										{subItem.label}
-									</Box>
-									<Box
-										fontSize="xs"
-										lineHeight="none"
-										color={{
-											base: 'gray-true.700',
-											_dark: 'gray-true.500',
-										}}
-									>
-										{subItem.value}
-									</Box>
-								</Box>
-							))}
-						</Grid>
-					</Flex>
+					<Item key={item.label} data={item} />
 				))}
 			</Flex>
 		</Box>
 	);
 }
 
-function getItems({search}: {search?: string}) {
-	const o: {
+interface IItem {
+	label: string;
+	value: {
 		label: string;
-		value: {
-			label: string;
-			value: string;
-		}[];
-	}[] = [];
+		value: string;
+	}[];
+}
+
+function Item({data}: {data: IItem}) {
+	const asJson = {
+		[data.label]: data.value.reduce<Record<string, unknown>>((o, i) => {
+			o[i.label] = i.value;
+			return o;
+		}, {}),
+	};
+
+	return (
+		<Flex
+			key={data.label}
+			flexDir={{
+				base: 'column',
+			}}
+			gap={{
+				base: '3',
+				lg: '2',
+			}}
+			className="group"
+		>
+			<Flex alignItems="center" gap="2">
+				<Box fontWeight="medium">{data.label}</Box>
+
+				<Clipboard
+					value={JSON.stringify(asJson, null, 2)}
+					display="flex"
+					transform="scale(0)"
+					transition="transform 200ms"
+					_groupHover={{
+						transform: 'scale(1)',
+					}}
+				>
+					<ClipboardTrigger cursor="pointer">
+						<ClipboardIndicator
+							copied={
+								<Icon w="4" h="4" color="success.500">
+									<CheckIcon />
+								</Icon>
+							}
+						>
+							<Icon w="4" h="4">
+								<Copy01Icon />
+							</Icon>
+						</ClipboardIndicator>
+					</ClipboardTrigger>
+				</Clipboard>
+			</Flex>
+
+			<Grid
+				gridTemplateColumns={{
+					base: 4,
+					md: 8,
+					lg: 12,
+				}}
+				gap="3"
+			>
+				{data.value.map((subItem) => (
+					<Box key={subItem.label}>
+						<Box
+							aspectRatio="square"
+							style={{
+								backgroundColor: subItem.value,
+							}}
+						/>
+
+						<Box mt="2" fontSize="sm">
+							{subItem.label}
+						</Box>
+						<Box
+							fontSize="xs"
+							lineHeight="none"
+							color={{
+								base: 'gray-true.700',
+								_dark: 'gray-true.500',
+							}}
+						>
+							{subItem.value}
+						</Box>
+					</Box>
+				))}
+			</Grid>
+		</Flex>
+	);
+}
+
+function getItems({search}: {search?: string}) {
+	const o: IItem[] = [];
 
 	Object.entries(colors).forEach(([k, v]) => {
 		if (typeof v === 'string') return;
