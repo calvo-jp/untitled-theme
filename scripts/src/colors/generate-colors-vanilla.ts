558b5b036b5731122574db9e6f775dcedfb32857
jp calvo <calvojp92@gmail.com>
2024-07-07 15:32:42 +0800

    fix tab

--- scripts/src/colors/generate-colors-vanilla.ts
+++ scripts/src/colors/generate-colors-vanilla.ts
@@ -10,7 +10,7 @@ export async function generateColorsVanilla() {
 		await fs.mkdir(outdir, {recursive: true});
 	} catch {}
 
-	const content = `:root {\n${getColors()}\n}`;
+	const content = `:root {\n${getColors()}}`;
 	const destination = path.join(outdir, 'index.css');
 
 	await fs.writeFile(destination, content, 'utf-8');
@@ -21,10 +21,10 @@ function getColors() {
 
 	Object.entries(colors).forEach(([k1, v1]) => {
 		if (typeof v1 === 'string') {
-			result += `--${k1}: ${v1};\n`;
+			result += `\t--${k1}: ${v1};\n`;
 		} else {
 			Object.entries(v1).forEach(([k2, v2]) => {
-				result += `--${k1}-${k2}: ${v2};\n`;
+				result += `\t--${k1}-${k2}: ${v2};\n`;
 			});
 		}
 	});
