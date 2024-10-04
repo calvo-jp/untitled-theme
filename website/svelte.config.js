9db01ebd167f86fbc1e7dfb62afe1142c523c7b1
jp calvo <calvojp92@gmail.com>
2024-10-04 17:51:00 +0800

    chore: use static adapter

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,12 +1,14 @@
 // @ts-check
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: 'index.html',
+		}),
 	},
 	compilerOptions: {
 		runes: true,
