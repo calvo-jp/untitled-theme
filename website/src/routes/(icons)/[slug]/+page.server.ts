b0356c985677f848028021265fd923ac6b0f3b01
jp calvo <calvojp92@gmail.com>
2024-12-12 09:45:30 +0800

    fix(website): duplicate cache control header

--- website/src/routes/(icons)/[slug]/+page.server.ts
+++ website/src/routes/(icons)/[slug]/+page.server.ts
@@ -2,9 +2,7 @@ import {getIcon} from '$lib/icons';
 import {error} from '@sveltejs/kit';
 import type {PageServerLoad} from './$types';
 
-export const load: PageServerLoad = async ({params, setHeaders}) => {
-  setHeaders({'Cache-Control': 'public, max-age=28800'});
-
+export const load: PageServerLoad = async ({params}) => {
   const icon = params.slug ? await getIcon(params.slug) : null;
 
   if (!icon) return error(404);
