e5f306e5d08aed97e3d77d1844c8dcf19dd4a742
jp calvo <calvojp92@gmail.com>
2024-12-22 22:15:19 +0800

    fix(website): enable system theme detection

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -11,7 +11,7 @@
   let {children} = $props();
 </script>
 
-<ThemeProvider>
+<ThemeProvider system>
   <Navbar />
   <main class="mx-auto max-w-screen-lg p-4 md:p-8 lg:p-12">
     {@render children()}
