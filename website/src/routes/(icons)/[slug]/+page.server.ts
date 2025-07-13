f04977905ba4bd596be1afd5d49f451340d2c2c9
jp calvo <calvojp92@gmail.com>
2025-07-13 14:00:00 +0800

    revert: "cache icon route"

--- website/src/routes/(icons)/[slug]/+page.server.ts
+++ website/src/routes/(icons)/[slug]/+page.server.ts
@@ -1,10 +1,7 @@
-import {dev} from '$app/environment';
 import {error} from '@sveltejs/kit';
 import type {PageServerLoad} from './$types';
 
-export const load: PageServerLoad = async ({parent, setHeaders}) => {
-  if (!dev) setHeaders({'cache-control': 'public, max-age=2592000'});
-
+export const load: PageServerLoad = async ({parent}) => {
   const {icon} = await parent();
 
   return icon ? {icon} : error(404);
