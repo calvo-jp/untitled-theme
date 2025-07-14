03ddc3fa7cf314eaeb9db5583086c2a70677c75c
jp calvo <calvojp92@gmail.com>
2025-07-14 15:03:09 +0800

    rm test for now

--- scripts/tests/icons-solid.test.ts
+++
@@ -1,9 +0,0 @@
-import {generateIconsSolid} from '../src/icons/generate-icons-solid.js';
-
-describe('Solid Icons Generator', () => {
-	it('generates solid icons', async () => {
-		await generateIconsSolid();
-	});
-});
-
-function isValidSolidComponent(content: string) {}
